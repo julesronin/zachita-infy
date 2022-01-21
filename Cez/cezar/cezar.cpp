4ccb31e1efefc770e906f7b7f754004c7524e18c
julesronin <julesronin7@gmail.com>
2022-01-22 02:52:29 +0300

    labs

---
+++ Cez/cezar/cezar.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+
+using namespace std;
+
+void count(string l)
+{
+    unordered_map<char, int> map;
+    int t = 0;
+    for (const char& x : l)
+    {
+        map[x]++;
+    }
+    for (auto& x : map)
+    {
+        cout << "Буква " << x.first << " встречается " << x.second << " раз(а); " << endl;
+    }
+}
+
+void count2(string q, string s)
+{
+    unordered_map<char, int> map1;
+    unordered_map<char, int> map2;
+    int i;
+    for (const char& x : q)
+    {
+        map1[x]++;
+    }
+    for (const char& t : s)
+    {
+        map2[t]++;
+    }
+        for (auto& x : map1)
+        {
+            for (auto& t : map2)
+            {
+                if (x.second == t.second)
+                {
+                    for (i = 0; i <= s.length(); i++)
+                    {
+                        if (s[i] >= 'a' && s[i] <= 'z' && s[i] >= 'A' && s[i] <= 'Z')
+                            s[i] = x.first;
+                    }
+                }
+                
+            }
+        }
+    cout << q;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    string s, l;
+    int n;
+    cout << "Введите текст для зашифровки с помощью шифра Цезаря: \n";
+    getline(cin, s);
+    cout << "Введите значение ключа: \n";
+    cin >> n;
+    l = s;
+    for (auto& x : s)
+    {
+        if (x >= 'a' && x <= 'z')
+            x = ((x - 'a' + n) % 26) + 'a';
+        else if (x >= 'A' && x <= 'Z')
+            x = ((x - 'A' + n) % 26) + 'A';
+    }
+    cout << "Зашифрованное сообщение: \n" << s;
+    cout << "\nПодсчитывание частоты появления каждой буквы в исходном тексте: \n";
+    count(l);
+    cout << "Подсчитывание частоты появления каждой буквы в зашифрованном тексте: \n";
+    count(s);
+    cout << "Расшифрованное сообщение: \n";
+    count2(l, s);
+}
---
+++ DiffHell/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long ab(long long q, long long n, long long p)
+{
+    long long num;
+    num = pow(q,n);
+    num = num % p;
+    return num;
+}
+
+int main()
+{
+    long long q, p, a, b, A, B;
+    setlocale(LC_ALL, "Rus");
+    cout << "Введите зачения двух открытых ключей: \n";
+    cout << "q = ";
+    cin >> q;
+    cout << "p = ";
+    cin >> p;
+    cout << "Секретный ключ Алисы: \n";
+    cin >> a;
+    cout << "Секретный ключ Боба: \n";
+    cin >> b;
+    cout << "Боб и Алиса открыто передают полученные результаты после вычислений: \n";
+    A = ab(q, a, p);
+    B = ab(q, b, p);
+    cout << "A = " << A << "\n";
+    cout << "B = " << B << "\n";
+    long long A1 = ab(B, a, p);
+    long long B1 = ab(A, b, p);
+    if (A1 == B1)
+    {
+        cout << "Общий секретный ключ: " << A1 << " \n";
+    }
+}
